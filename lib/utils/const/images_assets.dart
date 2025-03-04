ca37477138c4d36f8cb77d4e7c8fb4ed070f0c0f
GrullonDev <prosystem155@gmail.com>
2025-03-03 21:24:36 -0600

    feat: agregar clase ImageAssets para gestionar rutas de imágenes

---
+++ lib/utils/const/images_assets.dart
@@ -0,0 +1,30 @@
+class ImageAssets {
+  static const String profile = 'assets/images/profile.jpg';
+  static const String commingSoon = 'assets/images/commingSoon.png';
+  static const String inicio = 'assets/images/projects/portfolio/inicio.png';
+  static const String aboutMe1 =
+      'assets/images/projects/portfolio/about_me1.png';
+  static const String aboutMe2 =
+      'assets/images/projects/portfolio/about_me2.png';
+  static const String services1 =
+      'assets/images/projects/portfolio/services1.png';
+  static const String services2 =
+      'assets/images/projects/portfolio/services2.png';
+  static const String pomodoroBreak =
+      'assets/images/projects/pomodoro/break.png';
+  static const String pomodoroData = 'assets/images/projects/pomodoro/data.png';
+  static const String pomodoroHistory =
+      'assets/images/projects/pomodoro/history.png';
+  static const String pomodoroInit = 'assets/images/projects/pomodoro/init.png';
+  static const String pomodoroSession =
+      'assets/images/projects/pomodoro/session.png';
+  static const String english = 'assets/certificate/English.png';
+  static const String flutter = 'assets/certificate/flutter.png';
+  static const String frontend = 'assets/certificate/frontend.png';
+  static const String meditacion = 'assets/certificate/meditacion.png';
+  static const String feedback = 'assets/certificate/feedback.png';
+  static const String blandas = 'assets/certificate/blandas.png';
+  static const String marcaPersonal = 'assets/certificate/marcaPersonal.png';
+  static const String productividad = 'assets/certificate/productividad.png';
+  static const String videoPortafolio = 'assets/videos/portfolio.mov';
+}
